b0cf9e5dc4d4f3f52474e219d6b463d0f992cb7b
Sam Johnson <sam@scj.me>
2022-08-23 21:17:20 +0000

    Update styles to fix misc issues
    Co-authored-by: trogers-twilio <trogers@twilio.com>

--- src/components/CombinedRealtimeView/CombinedRealtimeView.jsx
+++ src/components/CombinedRealtimeView/CombinedRealtimeView.jsx
@@ -3,32 +3,51 @@ import styled from '@emotion/styled';
 import { QueuesStatsView, TeamsView } from '@twilio/flex-ui';
 
 const CombinedViewStyles = styled('div')`
-  position: relative;
-  overflow: hidden;
-  display: flex;
-  flex-flow: column nowrap;
+position: relative;
+overflow: hidden;
+display: flex;
+flex-flow: column nowrap;
+height: 100%;
+
+.stackedView {
+  overflow-y: scroll;
+  min-height: 50%;
+  max-height: 50%;
+}
+
+.Twilio-QueuesStatsView {
   height: 100%;
-  
-  .stackedView {
-    overflow-y: scroll;
-    min-height: 50%;
-    max-height: 50%;
-  }
-  
-  .Twilio-TeamsView-Loaded h2 {
-    display: none;
-  }
-  
-  .Twilio-TeamsView-Filter {
-    margin-top: 0;
-  }
-  
-  hr {
-    width: 100%;
-    border: 1px solid rgb(202, 205, 216);
-    border-bottom: none;
-  }
-  `;
+  padding: 1rem;
+}
+
+.Twilio-TeamsView {
+  height: 100%;
+}
+
+.Twilio-TeamsView-Loaded {
+  padding: 1rem;
+}
+
+.Twilio-TeamsView-Loaded h2 {
+  display: none;
+}
+
+.Twilio-TeamsView-Filter {
+  margin-top: 0;
+}
+
+.Twilio-TeamFiltersPanel {
+  height: 100%;
+}
+
+hr {
+  width: 100%;
+  border: 1px solid rgb(202, 205, 216);
+  border-bottom: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+}
+`;
 
 const CombinedRealtimeView = (props) => {
   return (
